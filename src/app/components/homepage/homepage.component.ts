a966e598026a4a198358ca5a1d298f3c7370053a
vani8 <vanisanaboina@gmail.com>
2021-11-03 14:42:42 +0530

    details

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit,Output, EventEmitter, Input  } from '@angular/core';
-import { Params, Router } from '@angular/router';
+import { Component, OnInit  } from '@angular/core';
+import { Router } from '@angular/router';
 import { BaseService } from '../../shared/base.service';
 import { CartService } from '../../shared/cart.service';
-import { ProductService } from '../../shared/product.service';
 
 
 @Component({
@@ -12,10 +11,9 @@ import { ProductService } from '../../shared/product.service';
 })
 export class HomepageComponent implements OnInit {
   public productList : any;
-  public  message:any;
   public productdetail: any;
   public totalItem: number =0;
-  constructor(private base: BaseService,private router: Router,private cartService: CartService,private api:ProductService) { }
+  constructor(private base: BaseService,private router: Router,private cartService: CartService) { }
 
   ngOnInit(): void {
 
@@ -32,12 +30,11 @@ export class HomepageComponent implements OnInit {
   // public open(_id:any){
   //   this.router.navigate(['/productlist,_id']);
   // }
- 
   addtocart(item: any){
     this.cartService.addtoCart(item);
   }
-  getItem(item:any){
-    this.message = this.cartService.selectItem(item);
-    console.log(item)
+ 
+  getProductDetails(item: any) {
+    this.router.navigateByUrl('/productlist', { state: item });
   }
 }
--- src/app/components/productlist/productlist.component.ts
+++ src/app/components/productlist/productlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+// import { ActivatedRoute,Params } from '@angular/router';
 import { BaseService } from '../../shared/base.service';
 import { CartService } from '../../shared/cart.service';
 
@@ -9,17 +10,36 @@ import { CartService } from '../../shared/cart.service';
   styleUrls: ['./productlist.component.css']
 })
 export class ProductlistComponent implements OnInit {
-  @Input() data:string=""
-  public message : any; 
-  public productdetail: any;
+  public productList : any;
   public totalItem: number = 0;
   constructor(private base:BaseService,private cartService:CartService) { }
-
+  public productDetails?: ProductDetails;
   ngOnInit(): void {
-    
+    this.base.getProduct().subscribe(res=>{
+      this.productList = res;  
+    })
+    this.cartService.getProducts().subscribe(res=>{
+      this.totalItem = res.length;
+    })
+    this.productDetails = history.state;
+    console.log(this.productDetails);
   }
- 
-
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
+  }
+}
+  export interface ProductDetails {
+    category: string;
+    description: string;
+    id: number;
+    image: string;
+    navigationId: number;
+    price: number;
+    quantity: number;
+    rating: { rate: number; count: number };
+    title: string;
+    total: number;
+  }
   // on(id: any){
   //   let x:any
   //   return this.productList.filter(x=> x.id === id);
@@ -30,4 +50,4 @@ export class ProductlistComponent implements OnInit {
   //   console.log(item)
   // }
   
-}
+
